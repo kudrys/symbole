0410fb0610203e1c757775deba2297761bcefc4a
rych <ryskuduk@gmail.com>
2015-05-09 13:15:09 +0200

    .

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -1,8 +1,9 @@
 #include "symbol.h"
 #include <iostream>
+using namespace std;
 
 symbol::symbol(){
-    //ctor
+    counter =1;
 }
 
 symbol::~symbol(){
@@ -15,5 +16,19 @@ bool symbol::create_tab(){
     for (int i = 0; i < x+2; i++) {
         tab[i] = new bool [y+2];
     }
+}
+
+void symbol::draw(){
+     for(int i=0; i<y; i++) {
+        cout<<"\n";
+        for(int j=0; j<x; j++) {
+            cout<<tab[i][j];
+        }
+    }
+}
+
+void symbol::counter_grow(){
+    counter+=counter+1;
+
 }
 
